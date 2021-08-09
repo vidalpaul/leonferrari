a95e6254844150d8be4823f1b23ff25eb70d41fc
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-08-09 18:35:36 -0300

    articles text

--- components/Year.tsx
+++ components/Year.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from '../styles/Components.module.css';
 
-const Year = (props: { year: string }) => {
-  return <div className={styles.year}>{props.year}</div>;
+const Year = (props: { article: {} }) => {
+  return (
+    <div className={styles.year}>
+      <h1>{props.article.title}</h1>
+      {props.article.text}
+    </div>
+  );
 };
 
 export default Year;
